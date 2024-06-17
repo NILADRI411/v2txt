765a462131325aa23f710c13e809b93b356adc0a
NILADRI411 <58443655+NILADRI411@users.noreply.github.com>
2024-06-17 17:27:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,36 @@
-import streamlit as st
-import pyttsx3
+mport streamlit as st
+from audio_recorder_streamlit import audio_recorder
+import base64
 import time
  
-# engine = pyttsx3.init()
-# voices = engine.getProperty('voices')
-# engine.setProperty('voice', voices[1].id)  # Change index to change voices
  
-def ask_question(question):
-    
-    engine = pyttsx3.init()
-    voices = engine.getProperty('voices')
-    engine.setProperty('voice', voices[1].id)
-    engine.say(question)
-    engine.runAndWait()
-    engine=None
-    time.sleep(3)  # Wait for 3 seconds before asking the next question
+def autoplay_audio(data):
+    # with open(file_path, "rb") as f:
+    #     data = f.read()
+        b64 = base64.b64encode(data).decode()
+        md = f"""
+<audio controls autoplay="true">
+<source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
+</audio>
+            """
+        st.markdown(
+            md,
+            unsafe_allow_html=True,
+        )
  
-def main():
-    st.title("Questionnaire")
-    st.write("Please answer the following questions:")
+for i in range(0, 2):
+    audio_bytes = audio_recorder(
+        key=f"audio_recorder_{i}",
+        text="Start Length",
+        recording_color="#e8b62c",
+        neutral_color="#6aa36f",
+        icon_name="user",
+        icon_size="6x",
+        energy_threshold=(-1.0, 1.0),
+        pause_threshold=3.0
+    )
  
-    questions = [
-        "What is your name?",
-        "What is your class?",
-        "What is your roll number?"
-    ]
- 
-    for q in questions:
-        ask_question(q)
- 
-if __name__ == "__main__":
-    main()
+    if audio_bytes:
+        st.audio(audio_bytes, format="audio/wav")
+        autoplay_audio(audio_bytes)
+    audio_bytes=None
